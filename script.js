574caec44c097ee71eebbeded5273e56252619c9
Jakub Rak <kubarak41@gmail.com>
2022-08-03 14:07:41 +0200

    Initialized site, creating nav bar and styled it, creating acconts variable in script

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const account1 = {
+  name: "Jan Kowalski",
+  login: "qwe",
+  password: "123",
+};
+const account2 = {
+  name: "Mateusz Bendarski",
+  login: "asd",
+  password: "321",
+};
+const account3 = {
+  name: "Magdalena Biernacz",
+  login: "zxc",
+  password: "987",
+};
+
+const accounts = [
+  account1,
+  account2,
+  account3,
+];
