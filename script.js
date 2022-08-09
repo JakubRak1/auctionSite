d82b0d464df644d11c457e7c37e38b3b6ba9c7ff
Jakub Rak <kubarak41@gmail.com>
2022-08-09 20:07:22 +0200

    Login Button logic added

--- script.js
+++ script.js
@@ -1,48 +1,167 @@
 "use strict";
+// Data
+const main = () => {
+  const account1 = {
+    name: "Jan Kowalski",
+    login: "qwe",
+    password: "123",
+    messages: [],
+    auctions: [],
+  };
+  const account2 = {
+    name: "Mateusz Bendarski",
+    login: "asd",
+    password: "321",
+    messages: [],
+    auctions: [],
+  };
+  const account3 = {
+    name: "Magdalena Biernacz",
+    login: "zxc",
+    password: "987",
+    messages: [],
+    auctions: [],
+  };
 
-const account1 = {
-  name: "Jan Kowalski",
-  login: "qwe",
-  password: "123",
-};
-const account2 = {
-  name: "Mateusz Bendarski",
-  login: "asd",
-  password: "321",
-};
-const account3 = {
-  name: "Magdalena Biernacz",
-  login: "zxc",
-  password: "987",
-};
+  const accounts = [
+    account1,
+    account2,
+    account3,
+  ];
+  let mainApp =
+    document.querySelector("#main");
+  let currentAccount;
+  // DOM Elements//
+  //Log in elements
+  const logBtn = document.querySelector(
+    "#login_btn"
+  );
+  const logName =
+    document.querySelector(
+      ".login_input--login"
+    );
+  const logPass =
+    document.querySelector(
+      ".login_input--password"
+    );
+  const welcomeText =
+    document.querySelector(
+      ".welcome_text"
+    );
+  //Lang buttom
+  const lang =
+    document.querySelector(".languge");
+  //Event listner//
 
-const accounts = [
-  account1,
-  account2,
-  account3,
-];
-
-const lang =
-  document.querySelector(".languge");
-const langPng = document.querySelector(
-  ".lang_png"
-);
-const langTxt = document.querySelector(
-  ".lang_info"
-);
-
-langPng.addEventListener(
-  "click",
-  function () {
-    if (langTxt.value === "ENG") {
-      console.log("zmien");
-      langTxt.innerHTML = "PL";
+  logBtn.addEventListener(
+    "click",
+    (e) => {
+      e.preventDefault();
+      logIn();
     }
-  }
-);
+  );
 
-// for future pop up window
-{
+  lang.addEventListener(
+    "mouseover",
+    () => {
+      selectBtnPng(
+        "lang_png",
+        "lang_png_over"
+      );
+      selectBtnPng(
+        "lang_info",
+        "lang_text_over"
+      );
+    }
+  );
+
+  lang.addEventListener(
+    "mouseout",
+    () => {
+      disSelectBtnPng(
+        "lang_png",
+        "lang_png_over"
+      );
+      disSelectBtnPng(
+        "lang_info",
+        "lang_text_over"
+      );
+    }
+  );
+
+  //Functions//
+
+  const logIn = () => {
+    currentAccount = accounts.find(
+      (acc) =>
+        acc.login === logName.value
+    );
+    if (
+      +currentAccount?.password ===
+      +logPass.value
+    ) {
+      welcomeText.textContent =
+        currentAccount.name;
+      mainApp.classList.toggle(
+        "disp_none"
+      );
+      mainApp.classList.toggle("app");
+      logName.value = logPass.value =
+        "";
+      logName.blur();
+      logPass.blur();
+    } else {
+      logName.value = logPass.value =
+        "";
+      logName.blur();
+      logPass.blur();
+    }
+  };
+
+  const selectBtnPng = (
+    selectedElementByClass,
+    className
+  ) => {
+    document
+      .querySelector(
+        `.${selectedElementByClass}`
+      )
+      .classList.add(`${className}`);
+  };
+  const disSelectBtnPng = (
+    selectedElementByClass,
+    className
+  ) => {
+    document
+      .querySelector(
+        `.${selectedElementByClass}`
+      )
+      .classList.remove(`${className}`);
+  };
+
+  mainApp.classList.toggle("disp_none");
+  mainApp.classList.toggle("app");
+  // const lang =
+  //   document.querySelector(".languge");
+  // const langPng = document.querySelector(
+  //   ".lang_png"
+  // );
+  // const langTxt = document.querySelector(
+  //   ".lang_info"
+  // );
+
+  // langPng.addEventListener(
+  //   "click",
+  //   function () {
+  //     if (langTxt.value === "ENG") {
+  //       console.log("zmien");
+  //       langTxt.innerHTML = "PL";
+  //     }
+  //   }
+  // );
+
+  // for future pop up window
+
   /* <div class="add_item">
           <p class="title_label">Add item on auctions</p>
           <div class="chose_category">
@@ -75,4 +194,5 @@ langPng.addEventListener(
           </div>
           <button class="add_itembtn">Add item on auction</button>
         </div> */
-}
+};
+main();
